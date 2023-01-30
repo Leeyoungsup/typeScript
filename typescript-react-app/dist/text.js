972811bfa4242263903de50a222be18adfd993ce
Leeyoungsup <liive29252@gmail.com>
2023-01-30 13:45:05 +0900

    Types of TS part one

---
+++ typescript-react-app/dist/text.js
@@ -0,0 +1,16 @@
+var playerNico = {
+    name: 'nico'
+};
+var playerLynn = {
+    name: 'lynn',
+    age: 12
+};
+function playerMaker(name) {
+    return {
+        name: name
+    };
+}
+var nico = playerMaker('nico');
+nico.age = 12;
+
+//# sourceMappingURL=text.js.map
